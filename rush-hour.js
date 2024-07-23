0817f28c43adc10adf920c44e690f62b6b8d3092
Wsinc <51157299+wsinc-git@users.noreply.github.com>
2024-07-23 14:02:04 -0300

    rush-hour.js

--- rush-hour.js
+++ rush-hour.js
@@ -534,7 +534,7 @@ function startCountdown(duration) {
 
     if (--timer < 0) {
       clearInterval(countdown);
-      timerDisplay.textContent = "Tempo encerrado!";
+      timerDisplay.textContent = "encerrado!";
     }
     },
     1000
