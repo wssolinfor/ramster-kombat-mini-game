40e8f03d9db685f84cb8039762da983fa1c0ae02
Wsinc <51157299+wsinc-git@users.noreply.github.com>
2024-09-03 17:52:59 -0300

    Update rush-hour.js

--- rush-hour.js
+++ rush-hour.js
@@ -542,7 +542,8 @@ function startCountdown(duration) {
 }
 
 let puzzles = {
-  "2024-09-03": "CCoooPDoBBoPDEAAoPoEKLMMFFKLooJJKNNN",
+  "2024-09-04": "DooBooDCCBoHooAAoHEEFooHooFGGGooFooo",
+  "2024-09-03": "DooBooDCCBoHooAAoHEEFooHooFGGGooFooo",
   "2024-09-02": "CCoooPDoBBoPDEAAoPoEKLMMFFKLooJJKNNN",
   "2024-09-01": "oobbbcoooodcowaadeowggdeowhfffsshooo",
   "2024-08-31": "wbbbdowoccdeaafodeoofghhsssgoozzogoo",
